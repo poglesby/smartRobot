bc0a22947458d4497879631feddde19fe4725dd0
Paul Grant <poglesbyg@gmail.com>
2015-09-16 14:54:10 +0200

    got rid of user-friends listing

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tweepy, time, sys
+import tweepy, time, sys, ctypes
 argfile = str(sys.argv[1])
+
+def Mbox(title, text, style):
+    ctypes.windll.user32.MessageBoxA(0, text, title, style)
 
 #enter the corresponding info from your Twitter application
 CONSUMER_KEY = '1mrDAlzvTxbJIgzXlYv2mObaE'
@@ -21,13 +24,15 @@ public_tweets=api.home_timeline()
 for tweet in public_tweets:
     print tweet.text
 
-user = api.get_user('thelugal')
-
-print user.screen_name
-print user.followers_count
-for friend in user.friends():
-    print friend.screen_name
+# user = api.get_user('thelugal')
+#
+# print user.screen_name
+# print user.followers_count
+# for friend in user.friends():
+#     print friend.screen_name
 
 for line in f:
     api.update_status(status=line)
     time.sleep(600) #Tweets every 10 minutes
+
+Mbox('TweetBot', 'The program is done',0)
